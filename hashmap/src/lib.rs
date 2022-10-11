692d5a823f596e95cc5efffec8ed1c5aff94b8ae
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2022-10-11 20:53:39 +0200

    Add example to hashmap

--- hashmap/src/lib.rs
+++ hashmap/src/lib.rs
@@ -1,4 +1,57 @@
 //! Left-right hashmap.
+//!
+//! # Examples
+//!
+//! This example shows that writes made by the writer are not visible to the
+//! reader until they are [flushed].
+//!
+//! [flushed]: Writer::flush
+//!
+//! ```
+//! let (mut writer, handle) = hashmap::new();
+//! let reader1 = handle.into_reader();
+//! let reader2 = reader1.clone();
+//!
+//! // Add a value to the writer's copy.
+//! writer.insert("key", "value");
+//! assert_eq!(writer.len(), 1);
+//!
+//! // The readers can't see the value yet.
+//! let read_guard1 = reader1.read();
+//! assert!(read_guard1.get("key").is_none());
+//! assert_eq!(read_guard1.len(), 0);
+//!
+//! let handle = std::thread::spawn(move || {
+//!     // After the writer flushes the changes become visible to the readers.
+//!     // This will block until all readers stopped reading the current
+//!     // version. This will happen once all read guard are dropped.
+//!     writer.flush();
+//! });
+//! #
+//! # // Yey sleep based concurrency!
+//! # std::thread::sleep(std::time::Duration::from_millis(10));
+//!
+//! // After the writer flushed the change they are visible to the readers.
+//! let read_guard2 = reader2.read();
+//! assert_eq!(read_guard2.get("key"), Some(&"value"));
+//! assert_eq!(read_guard2.len(), 1);
+//! drop(read_guard2);
+//!
+//! // However readers that started reading before the writes were flushes will
+//! // still see old values. This ensures the readers get a consistent view of
+//! // the map, albeit (slightly) outdated.
+//! assert!(read_guard1.get("key").is_none());
+//! assert_eq!(read_guard1.len(), 0);
+//! drop(read_guard1); // This unblocks the writer's call to flush.
+//!
+//! // To get an updated view simply drop the read guard and get another one.
+//! let read_guard1 = reader1.read();
+//! assert_eq!(read_guard1.get("key"), Some(&"value"));
+//! assert_eq!(read_guard1.len(), 1);
+//! drop(read_guard1);
+//!
+//! # handle.join().unwrap();
+//! ```
 
 use std::collections::hash_map::{HashMap, RandomState};
 use std::hash::{BuildHasher, Hash};
@@ -45,6 +98,34 @@ where
 /// Write access to the hashmap.
 ///
 /// Write access is limited to insertions and removals.
+///
+/// # Examples
+///
+/// This example shows that writes made by the writer are not visible to the
+/// reader until they are [flushed].
+///
+/// [flushed]: Writer::flush
+///
+/// ```
+/// let (mut writer, handle) = hashmap::new();
+/// let reader = handle.into_reader();
+///
+/// writer.insert("key", "value");
+/// assert_eq!(writer.len(), 1);
+///
+/// // The reader can't see the value yet.
+/// let r = reader.read();
+/// assert!(r.get("key").is_none());
+/// assert_eq!(r.len(), 0);
+/// drop(r);
+///
+/// // After the writer flushes the changes become visible to the readers.
+/// writer.flush();
+/// let r = reader.read();
+/// assert_eq!(r.get("key"), Some(&"value"));
+/// assert_eq!(r.len(), 1);
+/// drop(r);
+/// ```
 pub struct Writer<K, V, S = RandomState> {
     inner: left_right::Writer<HashMap<K, V, S>, Operation<K, V>>,
 }
